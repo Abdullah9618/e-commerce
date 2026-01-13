ca0f69830fbbea865b268f8e4b8abf72e4596eb9
Abdullah9618 <abdullah1hundred@gmail.com>
2026-01-13 19:20:04 +0500

    Fix Tailwind: enable @tailwindcss/vite with React plugin, remove duplicate Vite config, bundle CSS via main.jsx; defer Font Awesome

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CartProvider } from "./context/CartContext";
+import "./index.css";
+import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <CartProvider>
